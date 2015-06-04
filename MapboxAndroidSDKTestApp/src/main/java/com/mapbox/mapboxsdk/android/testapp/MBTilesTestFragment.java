0f4abeb2e88971b3369f21ba6820a4907dbfe343
Brad Leege <bleege@gmail.com>
2015-06-04 14:51:26 -0500

    #762 - Removing WebSourceTileLayer as it's not needed

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MBTilesTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MBTilesTestFragment.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import com.mapbox.mapboxsdk.tileprovider.tilesource.ITileLayer;
 import com.mapbox.mapboxsdk.tileprovider.tilesource.MBTilesLayer;
 import com.mapbox.mapboxsdk.tileprovider.tilesource.TileLayer;
-import com.mapbox.mapboxsdk.tileprovider.tilesource.WebSourceTileLayer;
 import com.mapbox.mapboxsdk.views.MapView;
 
 public class MBTilesTestFragment extends Fragment
@@ -20,22 +19,13 @@ public class MBTilesTestFragment extends Fragment
         MapView mv = (MapView) view.findViewById(R.id.mbTilesMap);
 
         TileLayer mbTileLayer = new MBTilesLayer(getActivity(), "test.MBTiles");
-        //            mv.setTileSource(mbTileLayer);
-        mv.setTileSource(new ITileLayer[] {
-                mbTileLayer, new WebSourceTileLayer("mapquest",
-                "http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png").setName(
-                "MapQuest Open Aerial")
-                .setAttribution("Tiles courtesy of MapQuest and OpenStreetMap contributors.")
-                .setMinimumZoomLevel(1)
-                .setMaximumZoomLevel(18)
-        });
+        mv.setTileSource(new ITileLayer[] {mbTileLayer});
 
         mv.setScrollableAreaLimit(mbTileLayer.getBoundingBox());
         mv.setMinZoomLevel(mv.getTileProvider().getMinimumZoomLevel());
         mv.setMaxZoomLevel(mv.getTileProvider().getMaximumZoomLevel());
         mv.setCenter(mv.getTileProvider().getCenterCoordinate());
         mv.setZoom(0);
-
 
         return view;
     }
